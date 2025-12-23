38af5e01de7f0c80de8b2a956b31bf2ee7ccb0a0
“ryohei-ebisawa” <xebisawa.ryohei@contract.isid.co.jp>
2025-12-23 16:56:58 +0900

    chore: add new dependency for OpenID4VP Swift library

--- Package.swift
+++ Package.swift
@@ -16,6 +16,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-log.git", from: "1.5.3"),
         .package(url: "https://github.com/dentsusoken/tw-lib-ios-iso18013-data-model.git", branch: "main"),
+		.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-siop-openid4vp-swift.git", exact: "0.1.1"),
 		],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
